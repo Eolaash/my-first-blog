642f8f9d19afa808ea9a8d84171adb18c898a16f
Eolaash <blackoutat@mail.ru>
2021-12-16 12:51:39 +0400

    My Django Girls app, first commit

---
+++ Testing.py
@@ -0,0 +1,15 @@
+import requests
+username = 'Eolaash'
+token = '8a82232388dc9cb35a66c41fd22641f2b3d78530'
+
+response = requests.get(
+    'https://www.pythonanywhere.com/api/v0/user/{username}/cpu/'.format(
+        username=username
+    ),
+    headers={'Authorization': 'Token {token}'.format(token=token)}
+)
+if response.status_code == 200:
+    print('CPU quota info:')
+    print(response.content)
+else:
+    print('Got unexpected status code {}: {!r}'.format(response.status_code, response.content))
